9d4f9add615686823bf87d1d922966273e01f39b
youhojung <74446224+youhojung@users.noreply.github.com>
2025-03-30 20:40:31 +0900

    3rd commit
    신호등 비전 컨트롤 기능 추가.

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,36 +1,39 @@
 #include <Arduino.h>
 #include <TaskScheduler.h>
 #include <PinChangeInterrupt.h>
-
-#define LED_R 11  // LED가 연결된 핀
-#define LED_Y 10  // LED가 연결된 핀
-#define LED_G 9  // LED가 연결된 핀
+// LED 핀 번호
+#define LED_R 11
+#define LED_Y 10
+#define LED_G 9
+// 버튼 핀 번호
 #define BUTTON1 7
 #define BUTTON2 6
 #define BUTTON3 5
+// 가변저항 핀 번호
 #define POT A0
-
 int brightness = 0;    // LED 밝기 변수
-int periodRed=2000;
-int periodYel=500;
-int periodGre=2000;
-static bool BlinkG = false;
+int periodRed=2000;    // 빨간 LED 점멸 주기
+int periodYel=500;   // 노란 LED 점멸 주기
+int periodGre=2000;   // 초록 LED 점멸 주기
+static bool BlinkG = false;     // 초록 LED 점멸 상태 관련 변수. true: LED가 켜져있음, false: LED가 꺼져있음
+// 현재 Mode에 관한 변수. true면 활성화, false면 비활성화
 volatile bool Normal = true;
 volatile bool Emergency = false;
 volatile bool BlinkAll = false;
 volatile bool PowerOff = false;
+// 버튼 입력 관련 변수. true면 버튼이 눌렸다는 뜻
 volatile bool button1Pressed = false;
 volatile bool button2Pressed = false;
 volatile bool button3Pressed = false;
 
 Scheduler runner;   // TaskScheduler 객체 생성
 
-void Red();       // LED 켜기 함수
+void Red();
 void Yel_1();
 void Gre_1();
 void Gre_2();
 void Yel_2();
-void CheckSerialInput(); // Serial 입력 확인 함수
+void CheckSerialInput();
 void button1_ISR();
 void button2_ISR();
 void button3_ISR();
@@ -38,7 +41,7 @@ void emgc();
 void BlAl();
 void pwof();
 void SendBrightness();
-
+// TaskScheduler 객체 생성 및 Task 설정
 Task LEDR(0, TASK_ONCE, &Red, &runner, true);
 Task LEDY1(0, TASK_ONCE, &Yel_1, &runner, false);
 Task LEDG1(0, TASK_ONCE, &Gre_1, &runner, false);
@@ -49,14 +52,17 @@ Task BlinkAllTask(500, TASK_FOREVER, &BlAl, &runner, false);
 Task BrightnessTask(100, TASK_FOREVER, &SendBrightness, &runner, true);
 
 void setup() {
+    // LED 핀을 출력으로 설정
     pinMode(LED_R, OUTPUT);
     pinMode(LED_Y, OUTPUT);
     pinMode(LED_G, OUTPUT);
+    // 버튼 핀을 입력으로 설정
     pinMode(BUTTON1, INPUT_PULLUP);
     pinMode(BUTTON2, INPUT_PULLUP);
     pinMode(BUTTON3, INPUT_PULLUP);
+    // 시리얼 통신 시작
     Serial.begin(9600);
-
+    // 버튼 인터럽트 설정
     attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(BUTTON1), button1_ISR, FALLING);
     attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(BUTTON2), button2_ISR, FALLING);
     attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(BUTTON3), button3_ISR, FALLING);
@@ -64,78 +70,116 @@ void setup() {
 }
 
 void loop() {
+    // 가변저항은 0~1023의 값을 가지므로, 0~255로 변환
     brightness = analogRead(POT) / 4;
     runner.execute(); // TaskScheduler 실행
     if (button1Pressed) {
+        // 버튼 1이 눌렸을 때의 동작
+        // 버튼이 눌린 상태를 초기화
         button1Pressed = false;
+        // LEDG2의 반복 횟수를 6으로 설정
         LEDG2.setIterations(6);
-        if (Emergency == false) {
+        if (Emergency == false) { // Emergency가 비활성화 상태일 때
+            // Emergency 모드를 활성화하고, 나머지 모드를 비활성화
             Emergency = true;
             Normal = false;
             BlinkAll = false;
             PowerOff = false;
+            // 시리얼 통신으로 Emergency 모드 진입 알림
+            Serial.println("Emergency Mode");
+            // LEDG2의 반복 횟수를 0으로 만들고, 모든 Task 비활성화
+            LEDG2.setIterations(0);
             runner.disableAll();
+            // 필요한 기능만 다시 활성화
             SerialCheck.enable();
             BrightnessTask.enable();
             emgc();
-        } else {
+        } else { // Emergency가 활성화 상태일 때
+            // Emergency 모드를 비활성화하고, Normal 모드를 활성화
             Emergency = false;
             Normal = true;
+            // 시리얼 통신으로 Normal 모드 진입 알림
             Serial.println("Normal Mode");
+            // 다시 기본 LED 상태로 돌아가기
             LEDR.restart();
         }
     }
 
-    if (button2Pressed) {
+    if (button2Pressed) { // 버튼 2가 눌렸을 때의 동작
+        // 버튼이 눌린 상태를 초기화
         button2Pressed = false;
-        if (BlinkAll == false) {
+        if (BlinkAll == false) { // BlinkAll이 비활성화 상태일 때
+            // BlinkAll 모드를 활성화하고, 나머지 모드를 비활성화
             Emergency = false;
             Normal = false;
             BlinkAll = true;
             PowerOff = false;
-            runner.disableAll();
+            // 시리얼 통신으로 BlinkAll 모드 진입 알림
+            Serial.println("BlinkAll Mode");
+            // LEDG2의 반복 횟수를 0으로 만들고, 모든 Task를 비활성화
             LEDG2.setIterations(0);
+            runner.disableAll();
+            // 필요한 기능만 다시 활성화
             SerialCheck.enable();
             BrightnessTask.enable();
+            // BlinkAll 모드의 LED 상태 설정
             BlinkAllTask.restart();
-        } else {
+        } else { // BlinkAll이 활성화 상태일 때
+            // BlinkAll 모드를 비활성화하고, Normal 모드를 활성화
             PowerOff = false;
             Normal = true;
+            // BlinkAll을 실행하는 Task를 비활성화하고, LEDG2의 반복 횟수를 6으로 설정
             BlinkAllTask.disable();
             LEDG2.setIterations(6);
+            // 시리얼 통신으로 Normal 모드 진입 알림
             Serial.println("Normal Mode");
+            // LED를 모두 끈다.
             analogWrite(LED_R, 0);
             Serial.println("[" + String(millis()) + "] RED LED OFF");
             analogWrite(LED_G, 0);
             Serial.println("[" + String(millis()) + "] GREEN LED OFF");
             analogWrite(LED_Y, 0);
             Serial.println("[" + String(millis()) + "] YELLOW LED OFF");
+            // 기본 LED 상태로 돌아가기
             LEDR.restart();
         }
     }
 
-    if (button3Pressed) {
+    if (button3Pressed) { // 버튼 3이 눌렸을 때의 동작
+        // 버튼이 눌린 상태를 초기화
         button3Pressed = false;
-        LEDG2.setIterations(6);
-        if (PowerOff == false) {
+        // LEDG2의 반복 횟수를 0으로 설정정
+        LEDG2.setIterations(0);
+        if (PowerOff == false) { // PowerOff가 비활성화 상태일 때
+            // PowerOff 모드를 활성화하고, 나머지 모드를 비활성화
             Emergency = false;
             Normal = false;
             BlinkAll = false;
             PowerOff = true;
+            // 시리얼 통신으로 PowerOff 모드 진입 알림
+            Serial.println("PowerOff Mode");
+            // 모든 Task를 비활성화
             runner.disableAll();
+            // 필요한 기능만 다시 활성화
             SerialCheck.enable();
             BrightnessTask.enable();
+            // PowerOff 모드의 LED 상태 설정
             pwof();
-        } else {
+        } else { // PowerOff가 활성화 상태일 때
+            // PowerOff 모드를 비활성화하고, Normal 모드를 활성화
             PowerOff = false;
             Normal = true;
+            // 시리얼 통신으로 Normal 모드 진입 알림
             Serial.println("Normal Mode");
+            // LEDG2의 반복 횟수를 6으로 설정
+            LEDG2.setIterations(6);
+            // 기본 LED 상태로 돌아가기
             LEDR.restart();
         }
     }
 
 }
-
+// 빨간 LED를 켜는 함수
 void Red() {
     Serial.println("[" + String(millis()) + "] YELLOW LED OFF");
     analogWrite(LED_Y, 0);
@@ -143,7 +187,7 @@ void Red() {
     analogWrite(LED_R, brightness);
     LEDY1.restartDelayed(periodRed);
 }
-
+// 빨간 LED를 끄고, 노란 LED를 켜는 함수
 void Yel_1() {
     Serial.println("[" + String(millis()) + "] RED LED OFF");
     analogWrite(LED_R, 0);
@@ -151,76 +195,150 @@ void Yel_1() {
     analogWrite(LED_Y, brightness);
     LEDG1.restartDelayed(periodYel);
 }
-
+// 노란 LED를 끄고, 초록 LED를 켜는 함수
 void Gre_1() {
     Serial.println("[" + String(millis()) + "] YELLOW LED OFF");
     analogWrite(LED_Y, 0);
     Serial.println("[" + String(millis()) + "] GREEN LED ON");
     analogWrite(LED_G, brightness);
+    // 초록 LED 점멸 상태를 true로 설정
+    // 초록 LED가 켜져있는 상태로 넘어가기 때문에, LED를 끄기 위해 true로 설정
     BlinkG = true;
     LEDG2.restartDelayed(periodGre);
 }
-
+// 초록 LED를 점멸시키는 함수. 
 void Gre_2(){
-    if(BlinkG == true){
+    if(BlinkG == true){ // 초록 LED가 켜져있을 때
+        // LED를 끄고, 점멸 상태를 false로 설정
         analogWrite(LED_G, 0);
         Serial.println("[" + String(millis()) + "] GREEN LED OFF");
         BlinkG = false;
     }
-    else{
+    else{ // 초록 LED가 꺼져있을 때
+        // LED를 켜고, 점멸 상태를 true로 설정
         analogWrite(LED_G, brightness);
         Serial.println("[" + String(millis()) + "] GREEN LED ON");
         BlinkG = true;
     }
-    if (LEDG2.isLastIteration()) {
-        LEDY2.restartDelayed(170); // 노란 LED 켜기
+    if (LEDG2.isLastIteration()) { // 마지막 반복일 때
+        LEDY2.restartDelayed(170); // LEDY2로 넘어가기 위해 170ms 대기
+        // 166ms * 5 + 170ms = 1000ms이므로, 1초간 3번 깜빡이고 LEDY2로 넘어감
     }
 }
-
+// 초록 LED를 끄고, 노란 LED를 켜는 함수
 void Yel_2() {
     analogWrite(LED_G, 0);
     Serial.println("[" + String(millis()) + "] GREEN LED OFF");
     analogWrite(LED_Y, brightness);
     Serial.println("[" + String(millis()) + "] YELLOW LED ON");
+    // 다음 반복을 위해 BlinkG를 false로 설정
     BlinkG = false;
     LEDR.restartDelayed(periodYel);
 }
-
+// 시리얼 입력을 체크하는 함수
+// 시리얼 입력이 있을 때마다 호출됨
 void CheckSerialInput() {
-    if (Serial.available() > 0) {
-        String input = Serial.readStringUntil('\n');
+    if (Serial.available() > 0) { // 시리얼 입력이 있을 때
+        String input = Serial.readStringUntil('\n'); // '\n'까지 읽음
         input.trim(); // 공백 및 개행 문자 제거
 
-        if (input.length() > 1) { // 최소한 R/Y/G + 숫자 한 자리 이상 필요
-            char type = input.charAt(0); // 첫 번째 문자 추출
-            int value = input.substring(1).toInt(); // 숫자 부분 변환
+        if (input.length() > 1) { // 최소한 1글자 이상 입력되었을 때
+            if(input.startsWith("Finger")){ // "Finger"로 시작하는 경우
+                int value = input.substring(6).toInt(); // "Finger" 이후의 숫자 부분 변환
+                if(value == 0){ // 0일 때 Normal 모드로 전환
+                    // 이미 누른 버튼과 동일한 버튼을 눌렀을때와 동일한 기능능 
+                    Emergency = false;
+                    BlinkAll = false;
+                    PowerOff = false;
+                    Normal = true;
+                    LEDG2.setIterations(0);
+                    runner.disableAll();
+                    LEDG2.setIterations(6);
+                    SerialCheck.enable();
+                    BrightnessTask.enable();
+                    Serial.println("Normal Mode");
+                    analogWrite(LED_R, 0);
+                    Serial.println("[" + String(millis()) + "] RED LED OFF");
+                    analogWrite(LED_G, 0);
+                    Serial.println("[" + String(millis()) + "] GREEN LED OFF");
+                    analogWrite(LED_Y, 0);
+                    Serial.println("[" + String(millis()) + "] YELLOW LED OFF");
+                    LEDR.restart();
+                }
+                else if(value == 1){ // Emergency 모드로 전환.
+                    // Emergency 모드가 아닐 때 버튼 1을 눌렀을 때와 동일한 기능
+                    Emergency = true;
+                    Normal = false;
+                    BlinkAll = false;
+                    PowerOff = false;
+                    LEDG2.setIterations(0);
+                    runner.disableAll();
+                    LEDG2.setIterations(6);
+                    SerialCheck.enable();
+                    BrightnessTask.enable();
+                    Serial.println("Emergency Mode");
+                    emgc();
+                }
+                else if(value == 2){ // BlinkAll 모드로 전환.
+                    // BlinkAll 모드가 아닐 때 버튼 2를 눌렀을 때와 동일한 기능
+                    Emergency = false;
+                    Normal = false;
+                    BlinkAll = true;
+                    PowerOff = false;
+                    LEDG2.setIterations(0);
+                    runner.disableAll();
+                    LEDG2.setIterations(6);
+                    SerialCheck.enable();
+                    BrightnessTask.enable();
+                    Serial.println("BlinkAll Mode");
+                    BlinkAllTask.restart();
+                }
+                else if(value == 3){ // PowerOff 모드로 전환.
+                    // PowerOff 모드가 아닐 때 버튼 3을 눌렀을 때와 동일한 기능
+                    Emergency = false;
+                    Normal = false;
+                    BlinkAll = false;
+                    PowerOff = true;
+                    runner.disableAll();
+                    LEDG2.setIterations(0);
+                    SerialCheck.enable();
+                    BrightnessTask.enable();
+                    Serial.println("PowerOff Mode");
+                    pwof();
+                }
+            }
+            else{ // "Finger"로 시작하지 않는 경우
+                // 첫 번째 문자를 LED 종류로 판단하고, 나머지 부분을 숫자로 변환
+                char type = input.charAt(0); // 첫 번째 문자 추출
+                int value = input.substring(1).toInt(); // 숫자 부분 변환
 
-            if (value > 0) { // 유효한 값인지 확인
-                if (type == 'R') {
-                    periodRed = value;
-                    Serial.println("[" + String(millis()) + "] New periodRed: " + value);
-                } else if (type == 'Y') {
-                    periodYel = value;
-                    Serial.println("[" + String(millis()) + "] New periodYel: " + value);
-                } else if (type == 'G') {
-                    periodGre = value;
-                    Serial.println("[" + String(millis()) + "] New periodGre: " + value);
+                if (value > 0) { // 유효한 값인지 확인
+                    if (type == 'R') { // 빨간 LED의 주기 설정
+                        periodRed = value;
+                        Serial.println("[" + String(millis()) + "] New periodRed: " + value);
+                    } else if (type == 'Y') { // 노란 LED의 주기 설정
+                        periodYel = value;
+                        Serial.println("[" + String(millis()) + "] New periodYel: " + value);
+                    } else if (type == 'G') { // 초록 LED의 주기 설정
+                        periodGre = value;
+                        Serial.println("[" + String(millis()) + "] New periodGre: " + value);
+                    }
                 }
             }
         }
     }
 }
 
-void button1_ISR(){
+void button1_ISR(){ // 버튼 1 인터럽트 서비스 루틴
     button1Pressed = true;
 }
-void button2_ISR(){
+void button2_ISR(){ // 버튼 2 인터럽트 서비스 루틴
     button2Pressed = true;
 }
-void button3_ISR(){
+void button3_ISR(){ // 버튼 3 인터럽트 서비스 루틴
     button3Pressed = true;
 }
-
+// Emergency 모드에서 LED 상태 설정
 void emgc(){
     Serial.println("Emergency Mode");
     analogWrite(LED_Y, 0);
@@ -230,7 +348,7 @@ void emgc(){
     analogWrite(LED_R, brightness);
     Serial.println("[" + String(millis()) + "] RED LED ON");
 }
-
+// PowerOff 모드에서 LED 상태 설정
 void pwof(){
     Serial.println("PowerOff Mode");
     analogWrite(LED_Y, 0);
@@ -240,7 +358,7 @@ void pwof(){
     analogWrite(LED_R, 0);
     Serial.println("[" + String(millis()) + "] RED LED OFF");
 }
-
+// BlinkAll 모드에서 LED 상태 설정
 void BlAl(){
     Serial.println("BlinkAll Mode");
     static bool HL = false;
@@ -263,7 +381,7 @@ void BlAl(){
         HL = false;
     }
 }
-
+// LED 밝기를 시리얼로 전송하는 함수
 void SendBrightness() {
     Serial.println("Brightness: " + String(brightness));
 }
--- p5/sketch.js
+++ p5/sketch.js
@@ -1,69 +1,100 @@
 let port;
 let connectBtn;
-let slider;
+let sliderRed, sliderYellow, sliderGreen;
+let sliderBrightness;
 let circleRColor = 'gray';
 let circleYColor = 'gray';
 let circleGColor = 'gray';
 let periodRed = 2000;
-let periodYellow = 1000;
+let periodYellow = 500;
 let periodGreen = 2000;
-let newperiodRed = 2000;
-let newperiodYel = 500;
-let newperiodGre = 2000;
-let Mode = "Normal";
+let buttonState = "Normal";
 let Brightness = 0;
-let newstr = "";
+let Mode = "Normal";
+let video;
+let handPose;
+let hands = [];
+let thumbF, indexF, middleF, ringF, pinkyF;
+let fingerState;
+let preState;
+// handPose 모델 불러오기
+function preload() {
+  handPose = ml5.handPose();
+}
+// 손 검출 시작하고 콜백하기
+function gotHands(results) {
+  hands = results;
+}
 
 function setup() {
-  createCanvas(520, 500); //width, height
-  background('220');
-
-  port = createSerial(); // web serial control object
-
+  createCanvas(640, 600);
+  background(220);
+  // 비디오 기불러오기
+  video = createCapture(VIDEO, { flipped: true });
+  video.size(320, 180);
+  video.hide();
+  
+  handPose.detectStart(video, gotHands);
+  // 시리얼 포트 연결하기
+  port = createSerial();
   let usedPorts = usedSerialPorts();
   if (usedPorts.length > 0) {
     port.open(usedPorts[0], 9600);
   }
-
-  // Web serial connect button setting
+  // 아두이노 연결 버튼성생성
   connectBtn = createButton("Connect to Arduino");
-  connectBtn.position(60, 400);
+  connectBtn.position(375, 340);
   connectBtn.mousePressed(connectBtnClick);
-
-  // Create a slider and place it at the top of the canvas.
-  sliderR = createSlider(500, 5000, newperiodRed, 10);
-  sliderR.position(10, 10);
-  sliderR.size(500); // Set the width of the slider
-  sliderR.mouseReleased(changeSlider); 
-  
-  sliderY = createSlider(500, 5000, newperiodYel, 10);
-  sliderY.position(10, 25);
-  sliderY.size(500); // Set the width of the slider
-  sliderY.mouseReleased(changeSlider); 
-  
-  sliderG = createSlider(500, 5000, newperiodGre, 10);
-  sliderG.position(10, 40);
-  sliderG.size(500); // Set the width of the slider
-  sliderG.mouseReleased(changeSlider); 
-
-  textSize(18);
-  fill(0);
+  // 빨간 LED의 주기를 조절 가능한 슬라이더. 최소 1000, 최대 3000, 초기값 2000
+  sliderRed = createSlider(1000, 3000, periodRed, 10);
+  sliderRed.position(10, 190);
+  sliderRed.size(500);
+  sliderRed.mouseReleased(changeSliderRed); 
+  // 노란 LED의 주기를 조절 가능한 슬라이더. 최소 250, 최대 750, 초기값 500
+  sliderYellow = createSlider(250, 750, periodYellow, 10);
+  sliderYellow.position(10, 220);
+  sliderYellow.size(500);
+  sliderYellow.mouseReleased(changeSliderYellow); 
+  // 초록 LED의 주기를 조절 가능한 슬라이더. 최소 1000, 최대 3000, 초기값 2000
+  sliderGreen = createSlider(1000, 3000, periodGreen, 10);
+  sliderGreen.position(10, 250);
+  sliderGreen.size(500);
+  sliderGreen.mouseReleased(changeSliderGreen); 
+  // LED의 밝기를 보여주는 슬라이더. 조절 불가능.
+  sliderBrightness = createSlider(0, 255, 0, 1);
+  sliderBrightness.position(10, 280);
+  sliderBrightness.size(500);
+  sliderBrightness.attribute('disabled', '');
 }
 
 function draw() {
+  background(220);
+  // 빨간색, 노란색, 초록색 LED의 주기와 현재 모드, 현재 밝기를 출력
+  noStroke();
+  fill(0);
+  textSize(18);
+  text("Red Period: " + periodRed, 10, 420);
+  text("Yellow Period: " + periodYellow, 10, 440);
+  text("Green Period: " + periodGreen, 10, 460);
+  text("Button State: " + Mode, 10, 480);
+  text("Brightness: " + Brightness, 10, 500);
+  // LED의 상태를 나타내는 원을 색칠.
+  stroke(0);
+  fill(circleRColor);
+  circle(100, 350, 50);
+  fill(circleYColor);
+  circle(200, 350, 50);
+  fill(circleGColor);
+  circle(300, 350, 50);
+  // 시리얼 통신값 읽기
   let n = port.available(); 
   if (n > 0) {
     let str = port.readUntil("\n"); 
-    background(220);
     if(str.includes("[")){
       newstr = str;
     }
-    
     fill(0);
-    text("msg: " + newstr, 10, 200);
-    
-    
-    // Check for LED ON/OFF messages
+    // LED의 켜짐/꺼짐 신호를 받고, 그에 따라 색을 변화시킨다.
     if (str.includes("RED LED ON")) {
       circleRColor = 'red';
     } else if (str.includes("RED LED OFF")) {
@@ -81,6 +112,7 @@ function draw() {
     } else if (str.includes("GREEN LED OFF")) {
       circleGColor = 'gray';
     }
+    // 시리얼이 모드에 관한 통신이라면, 그 값을 Mode에 저장한다.
     if(str.includes("Emergency Mode")){
       Mode = "Emergency";
     }
@@ -93,7 +125,7 @@ function draw() {
     if(str.includes("Normal Mode")){
       Mode = "Normal";
     }
-    
+    // 시리얼 통신이 밝기에 관해있다면, Brightness에 저장한다.
     if (str.includes("Brightness: ")) {
       let brightnessStr = str.split("Brightness: ")[1].trim();
       if (!isNaN(brightnessStr) && brightnessStr.length > 0) {
@@ -101,33 +133,43 @@ function draw() {
       }
     }
   }
-
-  // Draw the circle
-  fill(circleRColor);
-  circle(100, 125, 50); // Centered circle with diameter 50
-  fill (circleYColor);
-  circle(175, 125, 50);
-  fill (circleGColor);
-  circle(250, 125, 50);
-  // Change button label based on connection status
-  if (!port.opened()) {
-    connectBtn.html("Connect to Arduino");
-  } else {
-    connectBtn.html("Disconnect");
+  // 비디오 불러오기
+  image(video, 0, 0);
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+    // 손의 각 keypoint에 빨간 점 그리기
+    for (let j = 0; j < hand.keypoints.length; j++) {
+      let keypoint = hand.keypoints[j];
+      fill(255, 0, 0);
+      noStroke();
+      circle(320 - keypoint.x, keypoint.y, 10);
+    }
+    // 필요한 keypoint 불러오기
+    let tip1 = hand.thumb_tip, mcp1 = hand.thumb_mcp, cmc1 = hand.thumb_cmc;
+    let tip2 = hand.index_finger_tip, pip2 = hand.index_finger_pip, mcp2 = hand.index_finger_mcp;
+    let tip3 = hand.middle_finger_tip, pip3 = hand.middle_finger_pip, mcp3 = hand.middle_finger_mcp;
+    let tip4 = hand.ring_finger_tip, pip4 = hand.ring_finger_pip, mcp4 = hand.ring_finger_mcp;
+    let tip5 = hand.pinky_finger_tip, pip5 = hand.pinky_finger_pip, mcp5 = hand.pinky_finger_mcp;
+    // 엄지를 제외한 손가락은 pip를 기준으로 꺾인다.
+    // 따라서 (tip.y - pip.y) * (pip.y - mcp.y)의 값이 양수라면 펴진 것이고
+    // (tip.y - pip.y) * (pip.y - mcp.y)의 값이 음수라면 구부러진 것이다.
+    // 엄지는 다른 손가락과 구동 방식이 달라 y축이 아닌 x축에서 dramatic한 변화가 있다.
+    // mcp와 cmc의 상대적 위치는 손을 구부리던지 펴던지 거의 변하지 않는다.
+    // 그러나 tip와 mcp의 상대적 위치는 크게 변한다.
+    // 따라서 (tip1.x - mcp1.x) * (mcp1.x - cmc1.x)가 양수면 펴져있을 것이고
+    // 그렇지 않다면 구부러져 있을 것이다.
+    thumbF = (tip1.x - mcp1.x) * (mcp1.x - cmc1.x) > 0 ? 1: 0;
+    indexF = (tip2.y - pip2.y) * (pip2.y - mcp2.y) > 0 ? 1 : 0;
+    middleF = (tip3.y - pip3.y) * (pip3.y - mcp3.y) > 0 ? 1 : 0;
+    ringF = (tip4.y - pip4.y) * (pip4.y - mcp4.y) > 0 ? 1 : 0;
+    pinkyF = (tip5.y - pip5.y) * (pip5.y - mcp5.y) > 0 ? 1 : 0;
+    // 제대로 구동이 되는지 확인하기 위한 콘솔 로그 작성
+    console.log(thumbF, indexF, middleF, ringF, pinkyF);
+    // fingerState의 변화를 알아보기
+    whatstate();
   }
-
-  fill(0);
-  text("Period Red : " + newperiodRed, 10, 250);
-  fill(0);
-  text("Period Yellow : " + newperiodYel, 10, 275);
-  fill(0);
-  text("Period Green : " + newperiodGre, 10, 300);
-  fill(0);
-  text("Mode : " + Mode, 10, 325);
-  fill(0);
-  text("Bright : " + Brightness, 10, 350);
 }
-
+// 버튼을 눌러 포트와 연결 혹은 연결 해제
 function connectBtnClick() {
   if (!port.opened()) {
     port.open(9600);
@@ -135,22 +177,44 @@ function connectBtnClick() {
     port.close();
   }
 }
-
-function changeSlider() {
-  newperiodRed = String(sliderR.value());
-  newperiodYel = String(sliderY.value());
-  newperiodGre = String(sliderG.value());
-  if(periodRed != newperiodRed){
-  periodRed = newperiodRed;
+// 빨간 LED의 슬라이더가 변했을 경우, 시리얼 통신을 보낸다.
+function changeSliderRed() {
+  periodRed = String(sliderRed.value());
   port.write("R" + periodRed + "\n");
-  }
-  if(periodYellow != newperiodYel){
-  periodYellow = newperiodYel;
+}
+// 노란 LED의 슬라이더가 변했을 경우, 시리얼 통신을 보낸다.
+function changeSliderYellow() {
+  periodYellow = String(sliderYellow.value());
   port.write("Y" + periodYellow + "\n");
-  }
-  if(periodGreen != newperiodGre){
-  periodGreen = newperiodGre;
+}
+//  LED의 슬라이더가 변했을 경우, 시리얼 통신을 보낸다.
+function changeSliderGreen() {
+  periodGreen = String(sliderGreen.value());
   port.write("G" + periodGreen + "\n");
+}
+
+function whatstate() { // 손가락의 상태에 따라 아두이노에 반환할 값을 정한다.
+  // fingerState는 손가락의 상태를 나타내는 변수
+  // fingerState는 각각 0, 1, 2, 3의 값을 가질 수 있다.
+  // 검지와 중지만 펴져있다면, 0을 반환
+  if (thumbF == 0 && indexF == 1 && middleF == 1 && ringF == 0 && pinkyF == 0) {
+    fingerState = 0;
+  }
+  // 모든 손가락이 펴져있다면, 1을 반환
+  if (thumbF == 1 && indexF == 1 && middleF == 1 && ringF == 1 && pinkyF == 1) {
+    fingerState = 1;
+  } 
+  // 검지, 중지, 약지만 펴져있다면, 2를 반환
+  if (thumbF == 0 && indexF == 1 && middleF == 1 && ringF == 1 && pinkyF == 0) {
+    fingerState = 2;
+  }
+  // 모든 손가락이 구부러져 있다면, 3을 반환
+  if (thumbF == 0 && indexF == 0 && middleF == 0 && ringF == 0 && pinkyF == 0) {
+    fingerState = 3;
+  }
+  // 불필요한 통신을 줄이기 위해 fingerState가 달라질 때에만 시리얼 통신을 보낸다.
+  if(preState != fingerState){
+    port.write("Finger" + fingerState + "\n");
+    preState = fingerState;
   }
 }
- 
